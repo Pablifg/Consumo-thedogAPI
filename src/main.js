c91791fee3bed07051774fa0dc02200845c8d242
Pablo Gallegos <pabli1128@hotmail.com>
2022-10-23 15:22:59 -0500

    Solución challenge: -Agregar botón que genere imagen aleatoria. - Se usó función asíncrona Async/Await

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 const URL = 'https://api.thedogapi.com/v1/images/search';
 
-fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    });
+const img = document.querySelector('img');
+const btn = document.querySelector('#btnGenerate');
+
+btn.addEventListener('click', generateImg);
+
+async function fetchData(urlAPI) {
+    const response = await fetch(urlAPI);
+    console.log(response);
+    const data = await response.json();
+    console.log(data[0])
+    //console.log(data[0].url);
+    img.src = data[0].url;
+}
+
+function generateImg() {
+    fetchData(URL);
+    //console.log('My button');
+}
