a926d7a28a5b7bc6bf0a2070cf799f1085b737d0
Pablo Gallegos <pabli1128@hotmail.com>
2022-10-24 16:47:38 -0500

    Usando endpoints y query parameters para limitar cantidad de img's

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-const URL = 'https://api.thedogapi.com/v1/images/search';
+//const URL = 'https://api.thedogapi.com/v1/images/search';
+//Ading query parameter
+const URL = 'https://api.thedogapi.com/v1/images/search?limit=3';
 
-const img = document.querySelector('img');
+const img1 = document.querySelector('#img1');
+const img2 = document.querySelector('#img2');
+const img3 = document.querySelector('#img3');
 const btn = document.querySelector('#btnGenerate');
 
 btn.addEventListener('click', generateImg);
@@ -9,9 +13,11 @@ async function fetchData(urlAPI) {
     const response = await fetch(urlAPI);
     console.log(response);
     const data = await response.json();
-    console.log(data[0])
+    console.log(data)
     //console.log(data[0].url);
-    img.src = data[0].url;
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
 function generateImg() {
